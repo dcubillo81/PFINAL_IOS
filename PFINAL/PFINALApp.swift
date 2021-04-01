081def7b0a56d42388154025e04a4737964d55bf
Daniel Cubillo <dcubillo81@gmail.com>
2021-04-01 08:34:55 -0600

    Initial Commit

---
+++ PFINAL/PFINALApp.swift
@@ -0,0 +1,17 @@
+//
+//  PFINALApp.swift
+//  PFINAL
+//
+//  Created by Daniel Cubillo on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PFINALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
